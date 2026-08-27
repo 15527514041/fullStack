018cf2ee87d9870b44c2bf0754e45b4a333b500a
Lan Guo An <guoan.l@runtech.io>
2026-08-27 09:25:32 +0800

    chore: CORS 白名单 + 新增 .env.example

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const { notFound, errorHandler } = require('./middlewares/errorHandler')
 const app = express()
 const port = process.env.PORT || 3008
 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173']
+}))
 app.use(express.json())
 
 app.get('/health', (req, res) => {
