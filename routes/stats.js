062900549d25655b97eaf4c3e2822f0de301830e
Lan Guo An <guoan.l@runtech.io>
2026-09-03 12:06:29 +0800

    fix: stats 路由路径报错

--- routes/stats.js
+++ routes/stats.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const statsController = require('../controllers/statsController');
-const auth = require('../middleware/auth');
+const auth = require('../middlewares/auth');
 
 router.use(auth);
 
-router.get('/todos-per-user', statsController.getTodoPerUser)
+router.get('/todos-per-user', statsController.getTodosPerUser);
 
 module.exports = router;
